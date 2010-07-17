70e9ede5c2fb76ed11214b2c8af49e30a2826a9c
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2010-07-17 13:14:03 +0200

    mved trunk to seriesly

--- trunk/helper/context_processors.py
+++
@@ -1,4 +0,0 @@
-from django.conf import settings
-
-def site_info(request):
-    return {'DOMAIN_URL': settings.DOMAIN_URL}
--- trunk/helper/string_utils.py
+++
@@ -1,5 +0,0 @@
-
-import re
-
-def normalize(s):
-    return re.sub("[^\w ]", "", s.lower())
