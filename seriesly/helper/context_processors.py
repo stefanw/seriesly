20f0c50ed6216ceeb47668dbde623de2bda89299
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2010-07-23 09:03:33 +0200

    secure domain as context processor

--- seriesly/helper/context_processors.py
+++ seriesly/helper/context_processors.py
@@ -1,4 +1,5 @@
 from django.conf import settings
 
 def site_info(request):
-    return {'DOMAIN_URL': settings.DOMAIN_URL}
+    return {'DOMAIN_URL': settings.DOMAIN_URL,
+            'SECURE_DOMAIN_URL': settings.SECURE_DOMAIN_URL}
