2e600c2cd87c2c8e30ca2010e9c8e05f3b799c28
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2010-10-01 11:19:59 +0200

    alternative to app engine url fetch

--- seriesly/helper/http.py
+++ seriesly/helper/http.py
@@ -3,7 +3,17 @@ try:
     from google.appengine.api.urlfetch import fetch as urlfetch_fetch
 except ImportError:
     logging.warn("There's no Appengine UrlFetch")
-    urlfetch_fetch = lambda x: x
+    def urlfetch_fetch(url, deadline=10):
+        import httplib
+        url = url[len("http://"):]
+        conn = httplib.HTTPConnection(url[:url.find("/")])
+        conn.request("GET", url[url.find("/"):])
+        response = conn.getresponse()
+        print response.status, response.reason
+        data = response.read()
+        return data
+    
+
 
 def get(url):
     response = urlfetch_fetch(url, deadline=10)
