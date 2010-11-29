f930ccd2099a44731485e5dd76f1b50bfb8b04b4
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2010-11-29 17:32:53 +0100

    better representation of ReleaseData

--- seriesly/releases/__init__.py
+++ seriesly/releases/__init__.py
@@ -10,4 +10,8 @@ class ReleaseDataClass(object):
         return "ReleaseData(**%s)" % str(self.kwargs)
 
 class ReleaseData(ReleaseDataClass):
-    pass
+    def __unicode__(self):
+        return u"ReleaseData(**%s)" % unicode(self.kwargs)
+    def __str__(self):
+        return "ReleaseData(**%s)" % str(self.kwargs)
+    __repr__ = __str__
