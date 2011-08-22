076729157665fd8d32aeb3feba409686ca66c09b
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2011-08-22 14:42:32 +0200

    Add seriously Template tags in series app

---
+++ seriesly/series/templatetags/serieslytags.py
@@ -0,0 +1,9 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def rfc3339(date): 
+    if date is None:
+        return ""
+    return date.strftime('%Y-%m-%dT%H:%M:%SZ')
