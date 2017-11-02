9b16c980a0b3ac627e8ba2c7ea88231a0f308bd9
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2017-11-02 22:29:04 +0100

    Upgrade celery config

--- seriesly/celery.py
+++ seriesly/celery.py
@@ -7,11 +7,9 @@ from celery import Celery
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seriesly.settings')
 
-from django.conf import settings  # noqa
-
 app = Celery('seriesly')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
-app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
