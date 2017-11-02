386e94d5122b4c5c70badb2cc6576c63b68dce09
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2017-11-02 22:28:18 +0100

    Fix form API

--- seriesly/subscription/forms.py
+++ seriesly/subscription/forms.py
@@ -23,7 +23,7 @@ class SerieslyCheckboxSelectMultiple(forms.CheckboxSelectMultiple):
         if value is None:
             value = []
         has_id = attrs and 'id' in attrs
-        final_attrs = self.build_attrs(attrs, name=name)
+        final_attrs = self.build_attrs(attrs)
         # Normalize to strings
         output = []
         str_values = set([force_text(v) for v in value])
