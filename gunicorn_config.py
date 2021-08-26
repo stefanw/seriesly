0d921ddc981939bb5d2579f863e47226b87332d3
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2021-08-26 14:27:27 +0200

    Remove gunicorn config

--- gunicorn_config.py
+++
@@ -1,9 +0,0 @@
-from psycogreen.gevent import patch_psycopg
-
-def post_fork(server, worker):
-    """
-    https://github.com/jneight/django-db-geventpool
-    """
-    patch_psycopg()
-    worker.log.info("Made Psycopg2 Green")
-
