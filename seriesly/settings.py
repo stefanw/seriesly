059d89ea47e748f412db6cac80ac7ac3787e37ac
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2021-08-26 10:15:50 +0200

    Move public folder out of code dir

--- seriesly/settings.py
+++ seriesly/settings.py
@@ -95,7 +95,7 @@ INSTALLED_APPS = [
 
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "public", "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "public", "static")
 
 # Additional locations of static files
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
